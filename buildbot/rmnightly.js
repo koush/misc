dca5ce16aba340b5fe1b310c3b91352712b7fd8a
Koushik Dutta <koushd@gmail.com>
2010-05-03 15:16:28 -0700

    Add ROM Manager support.

---
+++ buildbot/rmnightly.js
@@ -0,0 +1,14 @@
+{
+    "version": 1,
+    "homepage": "http://www.cyanogenmod.com/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3283920",
+    "roms": [{
+        "product": "CyanogenModNightly",
+        "name": "CyanogenMod-5-05032010-NIGHTLY-N1",
+        "modversion": "CyanogenMod-5-0-NIGHTLY-N1",
+        "summary": "Experimental",
+        "incremental": "05032010",
+        "urls": ["http://buildbot.teamdouche.net/nightly/cyanogen_passion-ota-05032010.zip"],
+        "device": "dummy"
+    }]
+}
--- buildbot/updatejson.py
+++ buildbot/updatejson.py
@@ -28,7 +28,7 @@ class AndroidBuild:
         return self.getBuildProp("ro.product.device")
 
     def getBuildDate(self):
-        bd = re.match(r"^CyanogenMod-(\d{8})-NIGHTLY-\w*$", self.getModVersion())
+        bd = re.match(r"^CyanogenMod-5-(\d{8})-NIGHTLY-\w*$", self.getModVersion())
         if bd is not None:
             return bd.group(1)
 
@@ -53,6 +53,34 @@ class AndroidBuild:
             if (shouldAdd): oldJSON[device].insert(0, newEntry)
         return simplejson.dumps(oldJSON)
 
+    def dumpRMJSON(self, oldJSON):
+        oldJSON = simplejson.loads(oldJSON)
+        d = self.getDevice().split("_")
+        shouldAdd = True
+        modversion = self.getModVersion()
+
+        for device in d:
+            for rom in oldJSON['roms']:
+                if rom['modversion'] == modversion:
+                    shouldAdd = False
+            if shouldAdd:
+                newEntry = {
+                    "name": modversion,
+                    "modversion": modversion,
+                    "summary": "Experimental",
+                    "incremental": self.getBuildDate(),
+                    "product": "CyanogenModNightly",
+                    "device": device,
+                    "addons": [{
+                        "name": "Google Apps",
+                        "urls": ["http://koush.romraid.com//common/gapps-passion-EPE54B-signed.zip", "http://www.droidaftermarket.com/koush//common/gapps-passion-EPE54B-signed.zip", "http://droidk.macleodweb.net//common/gapps-passion-EPE54B-signed.zip", "http://android.antbox.org/koush//common/gapps-passion-EPE54B-signed.zip", "http://www.thekilpatrickproject.com/downloads/koush//common/gapps-passion-EPE54B-signed.zip", "http://briancrook.ca/android/nexus/gapps/gapps-passion-EPE54B-signed.zip"]
+                    }],
+                    "urls": [ "http://buildbot.teamdouche.net/nightly/%s" % self.getFilename() ]
+                }
+                oldJSON['roms'].insert(0, newEntry)
+
+        return simplejson.dumps(oldJSON)
+
     def moveFile(self, destinationdir):
         fullPath = destinationdir + "/" + self.getFilename()
         shutil.move(self.filename, fullPath)
@@ -71,6 +99,12 @@ def main():
         
         f = open("nightly.js", "w")
         f.write(ab.dumpJSON(oldJSON))
+        f.close()
+        
+        oldJSON = open("rmnightly.js").read()
+        #print ab.dumpRMJSON(oldJSON)
+        f = open("rmnightly.js", "w")
+        f.write(ab.dumpRMJSON(oldJSON))
         f.close()
 
 if __name__ == "__main__":
